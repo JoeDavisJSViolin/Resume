80e2407c5241a1abb26d2894cd92c74145ac628b
JoeDavisJSViolin <142959328+JoeDavisJSViolin@users.noreply.github.com>
2024-06-02 22:33:24 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ function openContent(evt, cityName) {
     }
   
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(cityName).style.display = "flex";
   evt.currentTarget.className += " active";
 }
 
